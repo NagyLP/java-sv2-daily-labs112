fd707ec2a4f3ef6e4ee3264fae143f8e364d740b
NagyLP <nagy.peter@pr.hu>
2022-01-14 15:49:49 +0100

    day05

--- src/main/java/day05/StringStatistics.java
+++ src/main/java/day05/StringStatistics.java
@@ -1,4 +1,20 @@
 package day05;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringStatistics {
+
+    private Map<Character, Integer> text = new HashMap<>();
+
+    public Map<Character, Integer> countChars(String text) {
+        for (char item : text.toCharArray()) {
+            if (this.text.containsKey(item)) {
+                this.text.put(item, this.text.get(item) + 1);
+            } else {
+                this.text.put(item, 1);
+            }
+        }
+        return this.text;
+    }
 }
